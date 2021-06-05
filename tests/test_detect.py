304318aa04e8d580e167fdf91dbf966f65bbf9ba
zafercavdar <zafercavdar@yahoo.com>
2021-06-05 18:42:10 +0300

    Added code base

---
+++ tests/test_detect.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from ftlangdetect import detect
+
+
+class TestDetect(TestCase):
+
+    def test_detect_base(self):
+        result = detect("")
+        assert "lang" in result
+        assert "score" in result
+        assert isinstance(result["lang"], str)
+        assert isinstance(result["score"], float)

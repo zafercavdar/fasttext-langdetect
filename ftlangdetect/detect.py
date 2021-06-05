2474ea6d145cbeacd5dbf57799f652bcc4f87306
zafercavdar <zafercavdar@yahoo.com>
2021-06-05 19:07:44 +0300

    Set max score to 1.0

--- ftlangdetect/detect.py
+++ ftlangdetect/detect.py
@@ -7,7 +7,7 @@ ft_model = fasttext.load_model("ftlangdetect/model/lid.176.bin")
 def detect(text: str) -> Dict[str, Union[str, float]]:
     labels, scores = ft_model.predict(text)
     label = labels[0].replace("__label__", '')
-    score = float(scores[0])
+    score = min(float(scores[0]), 1.0)
     return {
         "lang": label,
         "score": score,
